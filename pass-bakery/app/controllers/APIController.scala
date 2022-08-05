aec2827a26716ddb7c57b75a8028c147b6394dce
benjamingarza <benjisgarza@gmail.com>
2022-08-05 09:03:44 -0700

    Logic should be in Controller, not model. Added environment variable and the endpoint now returns the required format

--- pass-bakery/app/controllers/APIController.scala
+++ pass-bakery/app/controllers/APIController.scala
@@ -1,15 +1,43 @@
 package controllers
 
-import models._
+import io.circe.generic.extras.Configuration
+import io.circe.syntax.EncoderOps
 import javax.inject._
 import play.api._
 import play.api.mvc._
-
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Singleton
-class APIController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class APIController @Inject()(val environment: Environment, val controllerComponents: ControllerComponents) extends BaseController {
 
-  def echo() = Action { request =>
-    Ok(models.Status.getStatus)
+  def getEnvironment: String={
+    environment.mode.toString
   }
+
+  //get DateTime from server
+  def getDateTime: String ={
+    val dateTime: String =  LocalDateTime.now().toString
+    val formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+    val parsedDateTime: LocalDateTime = LocalDateTime.parse(dateTime,formatter)
+    parsedDateTime.toString
+  }
+
+  //Need json format
+  def getStatus: String ={
+    //We need to return this info
+    val status =("pass-bakery",getEnvironment,getDateTime)
+
+    //Set implicit value for encoder parameter (Otherwise we get "could not find implicit value" error when serializing)
+    implicit val customConfig: Configuration = {
+      Configuration.default.withDiscriminator("type")
+    }
+    //Serialize to Json
+    status.asJson.spaces2
+  }
+
+  def serviceStatus(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok(getStatus)
+  }
+
 }
--- pass-bakery/app/controllers/HomeController.scala
+++ pass-bakery/app/controllers/HomeController.scala
@@ -8,7 +8,6 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
