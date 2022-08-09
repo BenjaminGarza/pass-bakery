ddf0f81ddb76349ec12c70df704297220c06c44f
benjamingarza <benjisgarza@gmail.com>
2022-08-09 15:39:26 -0700

    Configured and ran scalafmt

--- app/models/APIModel.scala
+++ app/models/APIModel.scala
@@ -1,5 +1,3 @@
 package models
 
-case class Status(service: String, environment: String, serverTime: String) {
-
-}
+case class Status(service: String, environment: String, serverTime: String) {}
--- app/services/APIService.scala
+++ app/services/APIService.scala
@@ -4,13 +4,15 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 
-class APIService {
-}
+class APIService {}
 
-object APIService{
+object APIService {
   //get DateTime from server
-  def getDateTime: String ={
-    val parsedDateTime: LocalDateTime = LocalDateTime.parse(LocalDateTime.now().toString, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+  def getDateTime: String = {
+    val parsedDateTime: LocalDateTime = LocalDateTime.parse(
+      LocalDateTime.now().toString,
+      DateTimeFormatter.ISO_LOCAL_DATE_TIME
+    )
     //Truncate to remove nano seconds
     parsedDateTime.truncatedTo(ChronoUnit.SECONDS).toString
   }
