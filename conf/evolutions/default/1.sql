15a961e4af4884ebd96861e69f1cde708b1d5da8
benjamingarza <benjisgarza@gmail.com>
2022-08-19 09:34:18 -0700

    Made changes recommended in PR. Added readme and fixed evolutions

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,16 +2,13 @@
 
 -- !Ups
 
- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
  CREATE TABLE Product (
- id uuid DEFAULT uuid_generate_v4 (),
+ id uuid  NOT NULL PRIMARY KEY,
  name VARCHAR NOT NULL,
  quantity INT NOT NULL,
  price NUMERIC(6, 2) NOT NULL CHECK (price >= 0),
- created_at TIMESTAMP,
- updated_at TIMESTAMP,
- PRIMARY KEY (id)
+ created_at TIMESTAMP NOT NULL,
+ updated_at TIMESTAMP NOT NULL,
 );
 
 -- !Downs
