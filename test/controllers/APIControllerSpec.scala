6dc21558fc89b3b887be00410a8bbf9fd959528d
benjamingarza <benjisgarza@gmail.com>
2022-08-29 11:37:13 -0700

    Create test to verify a product can be added

--- test/controllers/APIControllerSpec.scala
+++ test/controllers/APIControllerSpec.scala
@@ -1,23 +1,50 @@
 package controllers
 
+import org.scalatest.MustMatchers.convertToAnyMustWrapper
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
-import play.api.test.Helpers.contentAsString
-import play.api.test.{FakeRequest, Injecting}
-
-import scala.concurrent.Future
-import play.api.mvc._
 import play.api.test.Helpers._
-
-import java.time.{LocalDate}
+import play.api.test.{FakeRequest, Injecting}
+import play.api.mvc._
+import java.time.LocalDate
+import scala.concurrent.Future
+import scala.language.postfixOps
 
 class APIControllerSpec
     extends PlaySpec
     with Results
     with GuiceOneAppPerTest
     with Injecting {
-
-  "APIController" should {
+  "Create product" should {
+    "Return successful" in {
+      val controller = inject[APIController]
+      val result: Future[Result] = controller
+        .postProduct()
+        .apply(
+          FakeRequest(
+            POST,
+            "/pass-bakery/status"
+          ).withTextBody(
+            text =
+              "{\"name\": \"crepe\", \"quantity\": \"5\", \"price\": \"4.99\"}"
+          )
+        )
+      val bodyText: String = contentAsString(result)
+      bodyText mustBe ("Post successful, 1 rows updated")
+    }
+  }
+  "findAllProducts" should {
+    "Should return Nil when table is empty" in {
+      val controller = inject[APIController]
+      val result: Future[Result] = controller
+        .findAllProducts()
+        .apply(FakeRequest(GET, "/rest/bakery"))
+      val bodyText: String = contentAsString(result)
+      status(result) mustBe 200
+      bodyText must include("Nil")
+    }
+  }
+  "Status endpoint" should {
     "Should return status" in {
       val controller = inject[APIController]
       val result: Future[Result] = controller
@@ -28,16 +55,6 @@ class APIControllerSpec
       bodyText must include("pass-bakery")
       bodyText must include("Test")
       bodyText must include(LocalDate.now().toString)
-    }
-
-    "Should return Nil when table is empty" in {
-      val controller = inject[APIController]
-      val result: Future[Result] = controller
-        .findAllProducts()
-        .apply(FakeRequest(GET, "/rest/bakery"))
-      val bodyText: String = contentAsString(result)
-      status(result) mustBe 200
-      bodyText must include("Nil")
     }
   }
 
