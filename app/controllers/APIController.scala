BenjaminGarza/pass-bakery

commit 7b2e32ef2889a000f1e70a1a1321e7735761ea2d

parseFromJson no longer parses the same value twice

app/controllers/APIController.scala
@@ -8,7 +8,6 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import DAO.BakeryDB
-import io.circe
 import models.{ProductFromJson, ServiceStatus}
 
 import java.util.UUID
@@ -30,26 +29,16 @@ class APIController @Inject() (
   }
 
   def parseFromJson(request: Request[AnyContent]): Option[ProductFromJson] = {
-    val parseResult: Option[ProductFromJson] = {
-      request.body.asJson match {
-        case None =>
+    val parseResult: Option[ProductFromJson] =
+      parser.decode[ProductFromJson](request.body.toString) match {
+        case error =>
           None
-        case Some(rawText) => {
-          circe.parser.parse(rawText.toString()) match {
-            case Left(failure) =>
-              None
-            case Right(json) =>
-              parser
-                .decode[ProductFromJson](json.toString()) match {
-                case Left(error) =>
-                  None
-                case Right(product) => Some(product)
-              }
-          }
+        case Right(product) => {
+          Some(product)
         }
       }
-    }
     parseResult
+
   }
 
   def serviceStatus(): Action[AnyContent] = Action {

commit a1d0eb0ca8412bdc32d44df51857d9e974f6aa63

Fixed parseFromJson regression from commit 7b2e32e, started work on changing tests to use application/json fake requests

app/controllers/APIController.scala
@@ -3,7 +3,6 @@ package controllers
 import services._
 import io.circe.syntax.EncoderOps
 import io.circe._
-
 import javax.inject._
 import play.api._
 import play.api.mvc._
@@ -28,10 +27,14 @@ class APIController @Inject() (
     status.asJson.spaces2
   }
 
-  def parseFromJson(request: Request[AnyContent]): Option[ProductFromJson] = {
+  def parseFromJson(
+      request: Request[AnyContent]
+  ): Option[ProductFromJson] = {
     val parseResult: Option[ProductFromJson] =
-      parser.decode[ProductFromJson](request.body.toString) match {
-        case error =>
+      parser.decode[ProductFromJson](
+        request.body.asInstanceOf[AnyContentAsJson].json.toString()
+      ) match {
+        case Left(error) =>
           None
         case Right(product) => {
           Some(product)

test/controllers/APIControllerSpec.scala
@@ -1,21 +1,16 @@
 package controllers
 
-import DAO.BakeryDB
-import models.DBTransactor
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.test.Helpers._
-import play.api.test.{FakeRequest, Injecting}
+import play.api.test.{FakeHeaders, FakeRequest, Injecting}
 import play.api.mvc._
 import java.time.LocalDate
 import java.util.UUID
 import scala.concurrent.Future
 import scala.language.postfixOps
 import play.api.db.Databases
-import play.api.db.Database
-import play.api.db.Databases
-import play.api.db.evolutions._
-import org.h2.tools.Server
+import play.api.libs.json.JsString
 
 class APIControllerSpec
     extends PlaySpec
@@ -29,17 +24,6 @@ class APIControllerSpec
   )
 
   "postProduct method" should {
-//    withDatabase { database =>
-//      val connection = database.getConnection()
-//      connection
-//        .prepareStatement("insert into test values (10, 'testing')")
-//        .execute()
-//
-//      connection
-//        .prepareStatement("")
-//        .executeQuery()
-//        .next()
-
     "Return successful when adding a product with all needed values" in {
       val controller = inject[APIController]
       val result: Future[Result] = controller
@@ -48,9 +32,10 @@ class APIControllerSpec
           FakeRequest(
             POST,
             "/pass-bakery/product"
-          ).withTextBody(
-            text =
+          ).withJsonBody(
+            JsString(
               "{\"name\": \"crepe\", \"quantity\": \"5\", \"price\": \"4.99\"}"
+            )
           )
         )
 